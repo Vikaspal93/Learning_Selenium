ea536213d402f4d4115520b7bc0a3a54cd1929f6
Vikas Pal <142507490+Vikaspal93@users.noreply.github.com>
2024-01-03 03:54:51 +0530

    Add files via upload

---
+++ Launching_Empty_Safari_Browsers.java
@@ -0,0 +1,13 @@
+package launching_Empty_Browsers;
+
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Launching_Empty_Safari_Browsers {
+	
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new SafariDriver();
+	}
+
+}
