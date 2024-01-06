01005e1fcf14cca5e897839d0f65fbc6dd87dd6a
Vikas.Pal <vikas.pal@Admins-MacBook-Pro.local>
2024-01-07 02:43:20 +0530

    Complete code

--- Selenium_Learning/src/learning_Synchronization/LearningExplicitWait.java
+++ Selenium_Learning/src/learning_Synchronization/LearningExplicitWait.java
@@ -2,13 +2,12 @@ package learning_Synchronization;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LearningExplicitWait {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/vikas.pal/Desktop/Selenium_weekend_Batch/chromedriver-mac-arm64/chromedriver");
 		ChromeDriver driver=new ChromeDriver();
@@ -22,7 +21,15 @@ public class LearningExplicitWait {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
 		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
 		
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.name("password")));
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.name("Password")));
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		
+//		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+		driver.findElement(By.xpath("//button[contains(@class,'o')]")).click();
+		
+		Thread.sleep(2000);
+		
+		driver.quit();
 		
 	}
 
