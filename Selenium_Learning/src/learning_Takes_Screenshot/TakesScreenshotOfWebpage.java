84ff24100d72d0bf0b7deac8a1ab7c35a02aa50b
Vikas.Pal <vikas.pal@Admins-MacBook-Pro.local>
2024-01-07 01:45:40 +0530

    complete take SS of webpage code

--- Selenium_Learning/src/learning_Takes_Screenshot/TakesScreenshotOfWebpage.java
+++ Selenium_Learning/src/learning_Takes_Screenshot/TakesScreenshotOfWebpage.java
@@ -1,10 +1,17 @@
 package learning_Takes_Screenshot;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class TakesScreenshotOfWebpage {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/vikas.pal/Desktop/Selenium_weekend_Batch/chromedriver-mac-arm64/chromedriver");
 		WebDriver driver=new ChromeDriver();
@@ -13,7 +20,10 @@ public class TakesScreenshotOfWebpage {
 		
 		driver.get("https://doubtnut.com/");
 		
-		 ts = driver
+		 TakesScreenshot ts = (TakesScreenshot) driver;
+		 File source = ts.getScreenshotAs(OutputType.FILE);
+		 File destination = new File("/Users/vikas.pal/git/Learning_Selenium/Selenium_Learning/screenshots/webpagess.png");
+		 FileHandler.copy(source, destination);
 		
 		
 	}
